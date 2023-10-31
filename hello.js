2c86aafdb89dad193e6e873a5d47d2ccab34f3cc
Ranko Sevo <ralesale@hotmail.com>
2023-10-31 01:50:51 +0100

    Update hello message

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+// const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Moon!";
 console.log(hello());
 exports.hello = hello;
